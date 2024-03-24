7af5486b75fd2c3eba0b10b2c394348d1e531790
Matheus-D-R-Costa <matheusdarosacosta@outlook.com>
2024-03-23 22:28:21 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ function updateLanguages(profileData) {
 }
 
 function updatePortfolio(profileData) {
-    const portfolio = document.getElementById(profile.portfolio)
+    const portfolio = document.getElementById('profile.portfolio')
     portfolio.innerHTML = profileData.portfolio.map(project => {
         return `
         <li>
